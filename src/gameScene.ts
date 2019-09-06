03507f086b13f3b9a450e161cc39ace0f23fbe8e
Eleanore <sebastien.andrys@gmail.com>
2019-09-06 12:40:13 +0200

    space is scrolling, everything ready to make game

--- src/gameScene.ts
+++ src/gameScene.ts
@@ -1,5 +1,8 @@
 import "phaser";
 export class GameScene extends Phaser.Scene {
+
+    background: Phaser.GameObjects.Image;
+    backgroundLoop: Phaser.GameObjects.Image;
 constructor() {
     super({
         key: "GameScene"
@@ -11,9 +14,21 @@ constructor() {
     }
   
     create(): void {
-        this.add.image(0,0, "background").setOrigin(0,0);
+        this.background = this.add.image(0,0, "background").setOrigin(0,0);
+        this.backgroundLoop = this.add.image(800, 0, "background").setOrigin(0,0);
     }
     update(): void {
-        // TODO
+        this.scrollScreen();
+    }
+
+    scrollScreen() {
+        this.background.x -= 4;
+        this.backgroundLoop.x -= 4;
+        if (this.background.x <= -800) {
+            this.background.x = 800
+        }
+        if (this.backgroundLoop.x <= -800) {
+            this.backgroundLoop.x = 800
+        }
     }
 };
