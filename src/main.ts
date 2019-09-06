e79221461f227ee0e2485f679df37243e8259031
Eleanore <sebastien.andrys@gmail.com>
2019-09-06 11:02:00 +0200

    assets for img test

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    backgroundColor: 0xD22C84,
     scene: {
         preload: preload,
         create: create,
@@ -23,12 +24,13 @@ let game = new Phaser.Game(config)
 
 function preload ()
 {
-    this.load.image('background', '../assets/background.jpg')
+    this.phaser.game.load.crossOrigin = "anonymous";
+    this.add.tileSprite('background', '../assets/background.jpg')
 }
 
 function create ()
 {
-    this.add.image(0,60, "background");
+    this.add.image(100,100, "background");
 }
 
 function update ()
