42045500fdbeb96f5e23c5feebff586c20b76e2e
GitPAV <pierre.vincs@gmail.com>
2019-10-10 02:03:15 +0200

    test

--- src/gameScene.ts
+++ src/gameScene.ts
@@ -1,19 +1,35 @@
 import "phaser";
 export class GameScene extends Phaser.Scene {
-constructor() {
-    super({
-        key: "GameScene"
-    });
-}
+
+    background: Phaser.GameObjects.Image;
+    backgroundLoop: Phaser.GameObjects.Image;
+
+    constructor() {
+        super({
+            key: "GameScene"
+        });
+    }
 
     preload(): void {
         this.load.image('background', '../assets/background.jpg');
     }
   
     create(): void {
-        this.add.image(0,0, "background").setOrigin(0,0);
+        this.background = this.add.image(0,0, "background").setOrigin(0,0);
+        this.backgroundLoop = this.add.image(800, 0, "background").setOrigin(0,0);
     }
     update(): void {
-        // TODO
+        this.scrollScreen();
+    }
+
+    scrollScreen() {
+        this.background.x -= 4;
+        this.backgroundLoop.x -= 4;
+        if (this.background.x <= -800) {
+            this.background.x = 800
+        }
+        if (this.backgroundLoop.x <= -800) {
+            this.backgroundLoop.x = 800
+        }
     }
 };
